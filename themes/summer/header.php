2fd36b7009ccc906812786ec969c7d072359e6f0
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-07-20 23:00:27 -0700

    update and add fresh page

--- themes/summer/header.php
+++ themes/summer/header.php
@@ -15,9 +15,12 @@
     <!-- Bootstrap theme -->
     <link href="css/bootstrap-theme.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="theme.css" rel="stylesheet">
-    
+    <!-- HarvestPosse summer theme stylesheet -->
+    <link href="themes/summer/css/theme.css" rel="stylesheet" type="text/css"/>
+ <link href="css/lightbox.css" rel="stylesheet" />
+ <script src="js/jquery-1.11.0.min.js"></script>
+ <script src="js/lightbox.min.js"></script>
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -27,6 +30,8 @@
   </head>
 
   <body role="document">
-    <div class="page-header"><h1>Harvest Posse Logo Goes Here</h1></div>
+    <header class="page-header">
+      <h1>Harvest Posse Logo Goes Here</h1>
+    </header>
 
 <?php include_once('includes/nav.php'); ?>
