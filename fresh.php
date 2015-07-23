f4cf95ecfdce12c79364a6ad0f1764b2c4f1360f
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-07-23 16:04:18 -0700

    change pages content

--- fresh.php
+++ fresh.php
@@ -9,46 +9,72 @@
         <div class="panel-heading">
           <h3 class="panel-title">Fresh List</h3>
         </div>
-        <div class="panel-body">
-          <p>See what's fresh:</p>
-        <article class="chapterOne">
-   <div role="main">
-     <a href="images/farmer.jpg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/farmer.jpg" alt="Fresh list"/>
-     </a>
+        <div class="row">
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image8.jpeg" data-lightbox="image-1" data-title="Fresh list" width="275" height="183">
-     <img src="images/image8.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image8.jpeg" alt="Fresh list" />
      </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image6.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image6.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image6.jpeg" alt="Fresh list" />
      </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image5.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image5.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image5.jpeg" alt="Fresh list" />
      </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image2.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image2.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image2.jpeg" alt="Fresh list" />
      </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image3.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image3.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image3.jpeg" alt="Fresh list" />
      </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image4.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image4.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image4.jpeg" alt="Fresh list" />
      </a>
-     <a href="images/image7.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image7.jpeg" alt="Fresh list" width="275" height="183"/>
-     </a>
+     </div>
+     
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image8.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image8.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image8.jpeg" alt="Fresh list" />
      </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
      <a href="images/image9.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image9.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img class="img-responsive" src="images/image9.jpeg" alt="Fresh list" />
      </a>
-     <a href="images/farm-stands.jpg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/farm-stands.jpg" alt="Fresh list" width="275" height="183"/>
+     </div>
+     </div>
+            
+             <div class="row">
+          <div class="col-md-3 col-sm-4 col-xs-6">
+     <a href="images/image8.jpeg" data-lightbox="image-1" data-title="Fresh list" width="275" height="183">
+     <img class="img-responsive" src="images/image8.jpeg" alt="Fresh list" />
      </a>
-   </div>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
+     <a href="images/image6.jpeg" data-lightbox="image-1" data-title="Fresh list">
+     <img class="img-responsive" src="images/image6.jpeg" alt="Fresh list" />
+     </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
+     <a href="images/image5.jpeg" data-lightbox="image-1" data-title="Fresh list">
+     <img class="img-responsive" src="images/image5.jpeg" alt="Fresh list" />
+     </a>
+     </div>
+     <div class="col-md-3 col-sm-4 col-xs-6">
+     <a href="images/image2.jpeg" data-lightbox="image-1" data-title="Fresh list">
+     <img class="img-responsive" src="images/image2.jpeg" alt="Fresh list" />
+     </a>
+     </div>
 
-</article>
       </div>
       </div>
 <?php
