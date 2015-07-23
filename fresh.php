5bf67ed05e5f124eb06e2dc6e975445593f94567
Akash Tolangee <atolangee@gmail.com>
2015-07-23 15:42:32 -0700

    Update fresh.php

--- fresh.php
+++ fresh.php
@@ -14,37 +14,37 @@
         <article class="chapterOne">
    <div role="main">
      <a href="images/farmer.jpg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/farmer.jpg" alt="Fresh list"/>
+     <img  class="img-responsive" src="images/farmer.jpg" alt="Fresh list"/>
      </a>
      <a href="images/image8.jpeg" data-lightbox="image-1" data-title="Fresh list" width="275" height="183">
-     <img src="images/image8.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img   class="img-responsive"src="images/image8.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image6.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image6.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img  class="img-responsive" src="images/image6.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image5.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image5.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img  class="img-responsive" src="images/image5.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image2.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image2.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img  class="img-responsive" src="images/image2.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image3.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image3.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img  class="img-responsive" src="images/image3.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image4.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image4.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img   class="img-responsive" src="images/image4.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image7.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image7.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img   class="img-responsive" src="images/image7.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image8.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image8.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img  class="img-responsive" src="images/image8.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/image9.jpeg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/image9.jpeg" alt="Fresh list" width="275" height="183"/>
+     <img  class="img-responsive" src="images/image9.jpeg" alt="Fresh list" width="275" height="183"/>
      </a>
      <a href="images/farm-stands.jpg" data-lightbox="image-1" data-title="Fresh list">
-     <img src="images/farm-stands.jpg" alt="Fresh list" width="275" height="183"/>
+     <img   class="img-responsive" src="images/farm-stands.jpg" alt="Fresh list" width="275" height="183"/>
      </a>
    </div>
 
