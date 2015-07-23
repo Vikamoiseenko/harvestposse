4dacc6b0d21819b415cd4f9a116d97646d0d9479
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-07-23 16:13:55 -0700

    changes pages

--- about.php
+++ about.php
@@ -10,14 +10,14 @@
           <h3 class="panel-title">About Us</h3>
         </div>
         <div class="panel-body">
-          <p>Abou Us and our mission</p>
+          <img class="img-responsive" src="http://www.brpres.org/FarmersMarketPics/BRPYouthFarmersMarket%20012.jpg" width="1120" height="500" alt="Harvest Posse" class="image-inline" title="Farmers Market" />
+<p style="text-align: center; ">First day Farmers Market in Seattle, 2015</p>
+<h3 style="text-align: center; ">About Us</h3>
 <div id="parent-fieldname-text"><p><span>The Neighborhood Farmers Market Alliance is committed to supporting and strengthening Washington's small family farm businesses by creating and operating vibrant, successful neighborhood farmers markets.</span></p>
 <p><span>To meet our mission we provide effective direct sales sites for our region's small farmers, while also educating consumers about local farm products and the importance of supporting and preserving local farmland. In addition, we have been at the forefront of advocating for low income benefits programs to ensure equal food access.</span></p>
 <p><strong>Timeline:</strong></p>
 <p><strong>2015</strong> - Our Harvest Posse Farmers Market was orgine by local volunteers from Seattle.</p>
-<p style="text-align: center; "><img src="http://www.brpres.org/FarmersMarketPics/BRPYouthFarmersMarket%20012.jpg" width="300"height="300" alt="Harvest Posse" class="image-inline" title="Farmers Market" /></p>
 
-<p style="text-align: center; ">First day Farmers Market in Seattle, 2015</p>
 <p><span><br /></span></p></div>          
           
         </div>
