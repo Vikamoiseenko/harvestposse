2a0cb4c07aee5f9b7bf76ba49b779f4a4f32836b
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-07-28 12:57:42 -0700

    implement written content

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
 ?>
 
     <div class="container theme-showcase" role="main">
-      <div class="panel panel-primary">
+      <div class="panel panel-default">
         <div class="panel-heading">
           <h3 class="panel-title">About Us</h3>
         </div>
@@ -13,14 +13,9 @@
           <img class="img-responsive" src="http://www.brpres.org/FarmersMarketPics/BRPYouthFarmersMarket%20012.jpg" width="1120" height="500" alt="Harvest Posse" class="image-inline" title="Farmers Market" />
 <p style="text-align: center; ">First day Farmers Market in Seattle, 2015</p>
 <h3 style="text-align: center; ">About Us</h3>
-<<<<<<< HEAD
-<div id="parent-fieldname-text"><p><span>The Neighborhood Farmers Market Alliance is committed to supporting and strengthening Washington's small family farm businesses by creating and operating vibrant, successful neighborhood farmers markets.</span></p>
-<p><span>To meet our mission we provide effective direct sales sites for our region's small farmers, while also educating consumers about local farm products and the importance of supporting and preserving local farmland. In addition, we have been at the forefront of advocating for low income benefits programs to ensure equal food access.</span></p>
-=======
 <div id="parent-fieldname-text"><p><span>The Neighborhood Farmers Market Alliance is committed to supporting and strengthening Washington's small family farm businesses by creating and operating vibrant, successful neighborhood farmers markets. We are simply another resource coming up underneath the Farmers Markets of Seattle and providing support and answers for the consumers in this beautiful city! </span></p>
 <p><span>We provide effective direct sales sites for our region's small farmers, while also educating consumers about local farm products and the importance of supporting and preserving local farmland. In addition, we have been at the forefront of advocating for low income benefits programs to ensure equal food access. We also provide answers to the typical consumers questions.</span></p>
 <p><span><a href="#">Here</a> you will find plenty of information regarding what is available this beautiful summer season at each Neighborhood Market!</span></p>
->>>>>>> victoria
 <p><strong>Timeline:</strong></p>
 <p><strong>2015</strong> - Our Harvest Posse Farmers Market was orgine by local volunteers from Seattle.</p>
 
