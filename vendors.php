5122359a4db414ee3ac465903760684df361698e
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-08-11 17:42:46 -0700

    change vendors page

--- vendors.php
+++ vendors.php
@@ -3,14 +3,29 @@
   include_once ("includes/globals.php");  
   get_header();
 ?>
+
     <div class="container theme-showcase" role="main">
-    <div class="row">
-    <div class="panel panel-primary">
-    <div class="panel-heading">
-    <h3 class="panel-title">U-District</h3>
-    </div>
-            <div class="panel-body">
-                <p> Alm Hill Gardens - flowers </p>
+      
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Vendors List</h3>
+        </div>
+        <div class="panel-body">
+        
+        <div class="row">
+           <!-- <div class="col-md-4"> -->
+                <div id="accordion" class="panel-group">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                            <a href="#first"
+                               data-toggle="collapse"
+                               data-parent="#accordion">U-District</a>
+                            </div>
+                        </div>
+                        <div class="panel-collapse collapse" id="first">
+                            <div class="panel-body">
+                                <p> Alm Hill Gardens - flowers </p>
                 <p>Blong's Garden - flowers, vegetables </p>
                 <p>Bloom Creek Cranberries</p>
                 <p>Booth Canyon Orchard</p>
@@ -21,19 +36,21 @@
                 <p> Growing Things Farm </p>
                 <p>Growing Washington - berries, vegetables </p>
                 <p>Hama Hama Shellfish </p>
+                            </div>
+                        </div>
+                    </div>
 
-            </div>
-          </div>
-    </div></div>
-        
-        <div class="container theme-showcase" role="main">
-        <div class="row">
-        <div class="panel panel-primary">
-        <div class="panel-heading">
-        <h3 class="panel-title">Capitol Hill</h3>
-        </div>
-            <div class="panel-body">
-              <p> Alvarez Farms - produce </p>
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <a href="#second"
+                                   data-toggle="collapse"
+                                   data-parent="#accordion">Capitol Hill</a>
+                            </div>
+                        </div>
+                        <div class="panel-collapse collapse" id="second">
+                            <div class="panel-body">
+                                <p> Alvarez Farms - produce </p>
               <p> Bloom Creek Cranberries </p>
               <p> Canales Produce </p>
               <p> Chao Chang Garden </p>
@@ -44,18 +61,21 @@
                 <p>Growing Washington - berries, vegetables</p>
                 <p>Loki Fish Company</p>
                 <p>Nature's Last Stand - pork, produce, and ready-to-eat items </p>
-            </div>
-          </div>
-        </div></div>
-      
-     <div class="container theme-showcase" role="main">    
-     <div class="row">
-     <div class="panel panel-primary">
-     <div class="panel-heading">
-     <h3 class="panel-title">West Seattle </h3>
-            </div>
-            <div class="panel-body">
-                <p>Alvarez Farms - produce</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <a href="#third"
+                                   data-toggle="collapse"
+                                   data-parent="#accordion">West Seattle</a>
+                            </div>
+                        </div>
+                        <div class="panel-collapse collapse" id="third">
+                            <div class="panel-body">
+                                <p>Alvarez Farms - produce</p>
                 <p>Blong's Garden - flowers, vegetables</p>
                 <p>Bloom Creek Cranberries</p>
                 <p>Botanica - nursery</p>
@@ -66,19 +86,20 @@
                 <p>Green Bow Farm LLC</p>
                 <p>Growing Washington - berries, vegetables</p>
                 <p>Loki Fish Company</p>
-                
-            </div>
-          </div>
-        </div></div>
-        
-     <div class="container theme-showcase" role="main">
-     <div class="row">
-     <div class="panel panel-primary">
-     <div class="panel-heading">
-     <h3 class="panel-title">Columbia City</h3>
-     </div>
-        <div class="panel-body">
-            <p>Alvarez Farms - produce</p>
+                            </div>
+                        </div>
+                    </div>
+                                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <a href="#third"
+                                   data-toggle="collapse"
+                                   data-parent="#accordion">Columbia City</a>
+                            </div>
+                        </div>
+                        <div class="panel-collapse collapse" id="third">
+                            <div class="panel-body">
+                                <p>Alvarez Farms - produce</p>
             <p>Bao Yang Garden - flowers and vegetables</p>
             <p>Canales Produce</p>
             <p>Cascadia Edible Landscapes</p>
@@ -89,19 +110,21 @@
             <p>Olsen Farms</p>
             <p>Samish Bay Cheese</p>
             <p>Seattle Tilth Produce</p> 
-        </div>
-      </div>  
-    </div>
-	</div>
-          
-     <div class="container theme-showcase" role="main">
-         <div class="row">
-          <div class="panel panel-primary">
-            <div class="panel-heading">
-              <h3 class="panel-title">Lake City</h3>
-            </div>
-            <div class="panel-body">
-                <p> Alvarez Farms - produce</p>
+                            </div>
+                        </div>
+                    </div>
+
+                                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <a href="#third"
+                                   data-toggle="collapse"
+                                   data-parent="#accordion">Lake City</a>
+                            </div>
+                        </div>
+                        <div class="panel-collapse collapse" id="third">
+                            <div class="panel-body">
+                                <p> Alvarez Farms - produce</p>
                 <p>Blong's Garden - flowers, vegetables</p>
                 <p>Collins Family Orchards</p>
                 <p>Family Pepper and Produce</p>
@@ -111,20 +134,22 @@
                 <p>Olsen Farms</p>
                 <p>Sky Valley Family Farm - poultry, eggs</p>
                 <p>White Heron Wine - rotating schedule</p>
+                            </div>
+                        </div>
+                    </div>
 
-            </div>
-          </div>
-        </div>
-      </div>
-         
-    <div class="container theme-showcase" role="main">     
-      <div class="row">
-      <div class="panel panel-primary">
-        <div class="panel-heading">
-          <h3 class="panel-title">Phinney </h3>
-            </div>
-            <div class="panel-body">
-                <p>Alvarez Farms - produce</p>
+
+                                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <a href="#third"
+                                   data-toggle="collapse"
+                                   data-parent="#accordion">Phinney</a>
+                            </div>
+                        </div>
+                        <div class="panel-collapse collapse" id="third">
+                            <div class="panel-body">
+                                <p>Alvarez Farms - produce</p>
                 <p>Chao Chang Garden</p>
                 <p>Collins Family Orchards</p>
                 <p>Hayton Farms Berries</p>
@@ -135,18 +160,21 @@
                 <p>Nelli Farms</p>
                 <p>Olsen Farms</p>
                 <p>Steel Wheel Farm</p>
-                
-            </div>
-          </div>
-        </div></div>
-    <div class="container theme-showcase" role="main">  
-     <div class="row">
-      <div class="panel panel-primary">
-        <div class="panel-heading">
-          <h3 class="panel-title">Magnolia</h3>
-        </div>
-        <div class="panel-body">
-            <p>Alvarez Farms - produce</p>
+                            </div>
+                        </div>
+                    </div>
+
+                                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <a href="#third"
+                                   data-toggle="collapse"
+                                   data-parent="#accordion">Magnolia</a>
+                            </div>
+                        </div>
+                        <div class="panel-collapse collapse" id="third">
+                            <div class="panel-body">
+                                <p>Alvarez Farms - produce</p>
             <p>Cascade Natural Honey</p>
             <p>Chao Chang Garden</p>
             <p>Collins Family Orchards</p>
@@ -155,12 +183,24 @@
             <p>Goose and Gander Farm</p>
             <p>Olsen Farms</p>
             <p> R Heritage Farm</p>
-            
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+           <!-- </div> -->
+
+
         </div>
-      </div>  
-    </div>
-</div>      
-     
+    
+    <script src="lib/jquery-ui-1.10.4/jquery-1.10.2.js"></script>
+    <script src="lib/jquery-ui-1.10.4/ui/minified/jquery-ui.min.js"></script>
+    <script src="lib/jquery-ui-1.10.4/ui/jquery.ui.core.js"></script>
+    <script src="lib/jquery-ui-1.10.4/ui/jquery.ui.widget.js"></script>
+    <script src="lib/jquery-ui-1.10.4/ui/jquery.ui.accordion.js"></script>
+    <script src="lib/bootstrap.min.js"></script>
+    <script src="lib/fabric.min.js"></script>
+                
 
 <?php
   get_footer();
