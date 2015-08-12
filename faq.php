b48e3817393a22fda76fcbcefc6c1d3f175b6c0c
Akash Tolangee <atolangee@gmail.com>
2015-08-11 17:17:36 -0700

    Update faq.php

--- faq.php
+++ faq.php
@@ -12,13 +12,24 @@
         <div class="panel-body">
           
 <p style="text-align: center; ">Frequently Asked Questions, 2015</p>
-<h3 style="text-align: center; ">FAQ</h3>
-<div id="parent-fieldname-text"><p><span>The Neighborhood Farmers Market Alliance is committed to supporting and strengthening Washington's small family farm businesses by creating and operating vibrant, successful neighborhood farmers markets.</span></p>
-<p><span>To meet our mission we provide effective direct sales sites for our region's small farmers, while also educating consumers about local farm products and the importance of supporting and preserving local farmland. In addition, we have been at the forefront of advocating for low income benefits programs to ensure equal food access.</span></p>
-<p><strong>Timeline:</strong></p>
-<p><strong>2015</strong> - Our Harvest Posse Farmers Market was orgine by local volunteers from Seattle.</p>
 
-<p><span><br /></span></p></div>          
+<div id="parent-fieldname-text">
+	<p> Q.	Are pets allowed in the markets? </p>
+	<p> A.	Absolutely! However, you will be asked to remove your pet if they are misbehaving or causing problems. Please be respectful to vendors and their food products, as well as other market attendants - have your pet on a leash, and please assure they are well behaved during your time at the neighborhood markets. </p>
+     <br>
+	<p> Q.	Why are other markets like Pikes Place and Ballard not on your site?</p>
+	<p> A.	The markets we have focused on are all a part of the Neighborhood Farmers Markets Alliance.</p>
+	 <br>
+	<p> Q.	Do the neighborhood markets accept EBT (SNAP/food stamps)? </p>
+	<p> A.	Yes! It's quite easy - at the market information booth, the market staff will swipe your EBT card for the amount you request. You will then be given wooden market tokens to buy anything except cut flowers, alcohol, and ready to eat foods. The tokens never expire and can be used at all 7 of our farmers markets at any time.
+	 <br>
+	<p> Q.	Do the neighborhood markets accept credit or debit cards? </p>
+	<p> A.	Many vendors and farmers at the various markets do accept credit/debit cards, but you should always bring cash to the market just to be prepared. </p>
+	 <br>
+	<p> Q.	How I become a vendor? </p>
+	<p> A.	Go here and fill out the form we have made available - someone will be in touch with you shortly.</p>	
+
+          
           
         </div>
       </div>
