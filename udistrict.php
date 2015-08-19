eca67a75276e6f04376708503a7e9967def90cac
Brian Baker <mrbrianbaker@users.noreply.github.com>
2015-08-18 17:17:28 -0700

    Adding tweet button

--- udistrict.php
+++ udistrict.php
@@ -55,6 +55,11 @@
       <div class="col-xs-2">
        <a href="https://foursquare.com/v/university-district-farmers-market/47e4d6fbf964a5204c4e1fe3" target="_blank"><img class="img-responsive" src="images/foursquare.png" alt="foursquare icon"></a>
       </div>
+      <div class="col-xs-2">
+       <a href="https://twitter.com/intent/tweet?button_hashtag=UDistrictFarmersMkt" class="twitter-hashtag-button">Tweet #UDistrictFarmersMkt</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+      </div>
+      
 
    		</div>
           </div>
