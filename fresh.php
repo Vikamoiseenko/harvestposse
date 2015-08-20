c6f3c1684083736418880681ee9a5485c5e7ee20
Brian Baker <mrbrianbaker@users.noreply.github.com>
2015-08-20 15:51:48 -0700

    fixed fresh list

--- fresh.php
+++ fresh.php
@@ -12,26 +12,101 @@
 	<div class="panel-body">	
 <div class="col-md-10 col-md-offset-1">
   <!-- Nav tabs -->
+  <div class="tabcordion tabs-left tabbable">
   <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-    <li role="presentation"><a href="#fresh" aria-controls="fresh" role="tab" data-toggle="tab">Fresh List</a></li>
-  </ul>
+    <li role="presentation" class="active"><a href="#summer" aria-controls="summer" role="tab" data-toggle="tab"><img src="images/summerfreshlist.png"></a></li>
+    <li role="presentation"><a href="#fall" aria-controls="fall" role="tab" data-toggle="tab"><img src="images/fallfreshlist.png"></a></li>
+    <li role="presentation"><a href="#winter" aria-controls="winter" role="tab" data-toggle="tab"><img src="images/winterfreshlist.png"></a></li>
+    <li role="presentation"><a href="#spring" aria-controls="spring" role="tab" data-toggle="tab"><img src="images/springfreshlist.png"></a></li>
+      </ul>
   <!-- Tab panes -->
   <div class="tab-content">
-    <div role="tabpanel" class="tab-pane fade in active" id="home"><h3>Home</h3>
-      <p>Rhoncus magna nec enim, et proin aliquet mid, porta magnis.</p></div>
-    <div role="tabpanel" class="tab-pane fade" id="profile"><h3>Profile</h3>
-      <p>Odio mattis, non ut! Porttitor nunc phasellus cras elementum.</p></div>
-    <div role="tabpanel" class="tab-pane fade" id="messages"><h3>Messages</h3>
-      <p>Enim hac tristique elementum, nec rhoncus porttitor sagittis cum.</p></div>
-    <div role="tabpanel" class="tab-pane fade" id="fresh">
-					<div class="col-md-2"></div>
-                  <div class="col-md-8"><h4>Fresh List</h4><a class="twitter-timeline"  href="https://twitter.com/search?q=%23SeattleFarmMkts%2C%20%23UDistrictFarmersMkt%2C%20%23WSeattleFarmersMkt%2C%20%23CapHillFarmersMkt%2C%20%23PhinneyFarmersMkt%2C%20%23MagnoliaFarmersMkt%2C%20%23ColCityFarmersMkt%2C%20%23LkCityFarmersMkt%2C%20%23SeattleFarmMktFresh%2C%20%23SeattleFreshList" data-widget-id="633792383939821569">Tweets about #SeattleFarmMkts, #UDistrictFarmersMkt, #WSeattleFarmersMkt, #CapHillFarmersMkt, #PhinneyFarmersMkt, #MagnoliaFarmersMkt, #ColCityFarmersMkt, #LkCityFarmersMkt, #SeattleFarmMktFresh, #SeattleFreshList</a>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
-				<div class="col-md-2"></div>
+    <div role="tabpanel" class="tab-pane fade in active" id="summer">
+	<div class="col-md-2">
+	<h3>Summer</h3>
+      <p><li>Bell Peppers</li>
+      	<li>Blackberries</li>
+        <li>Blueberries</li>
+        <li>Cantaloupe</li>
+        <li>Corn</li>
+        <li>Cucumbers</li>
+        <li>Eggplant</li>
+        <li>Figs</li>
+        <li>Grapes</li>
+        <li>Green Beans</li>
+        <li>Honeydew</li>
+        <li>Hot Peppers</li>
+        <li>Peaches</li>
+        <li>Pears</li>
+        <li>Tomatoes</li>
+        <li>Zucchini</li>
+        <li>Watermelon</li>
+        <li>Sweet Potatoes</li></p>
+		</div>
+		<div class="col-md-2"></div>
+		<div class="col-md-8"><h4>Fresh List Twitter</h4>
+		            <a class="twitter-timeline"  href="https://twitter.com/harvest_posse" data-widget-id="634484631673311232">Tweets by @harvest_posse</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
           </div>
+		
+		
+		
+		</div>
+    <div role="tabpanel" class="tab-pane fade" id="fall"><h3>Fall</h3>
+    <img src="images/image3-sml.png" alt="lettuce" align="right">
+      <p><li>Apples</li>
+      <li>Beets</li>
+      <li>Broccoli</li>
+      <li>Brussels Sprouts</li>
+      <li>Cabbage</li>
+      <li>Carrots</li>
+      <li>Cauliflower</li>
+      <li>Celery</li>
+      <li>Collard Greens</li>
+      <li>Pumpkin</li>
+      <li>Rutabagas</li>
+      <li>Sweet Potatoes</li></p></div>
+    <div role="tabpanel" class="tab-pane fade" id="winter"><h3>Winter</h3>
+    <img src="images/image4-sml.png" alt="lettuce" align="right">
+      <p><li>Apples</li>
+      <li>Beets</li>
+      <li>Bok Choy</li>
+      <li>Brussels Sprouts</li>
+      <li>Cabbage</li>
+      <li>Carrots</li>
+      <li>Cauliflower</li>
+      <li>Celery</li>
+      <li>Swiss Chard</li>
+      <li>Collard Grens</li>
+      <li>Grapefruit</li>
+      <li>Kale</li>
+      <li>Leeks</li>
+      <li>Lemons</li>
+      <li>Onions</li>
+      <li>Oranges</li>
+      <li>Parsnips</li>
+      <li>Potatoes</li>
+      <li>Turnips</li></p>
+      </div><div role="tabpanel" class="tab-pane fade" id="spring"><h3>Spring</h3>
+      <img src="images/image5-sml.png" alt="lettuce" align="right">
+      <p><li>Apricots</li>
+      <li>Artichokes</li>
+      <li>Arugular</li>
+      <li>Asparagus</li>
+      <li>Beets</li>
+      <li>Broccoli</li>
+      <li>Green Onions</li>
+      <li>Celery</li>
+      <li>Kale</li>
+      <li>Peas</li>
+      <li>Radishes</li>
+      <li>Rhubarb</li>
+      <li>Spinach</li>
+      <li>Squash</li>
+      <li>Strawberries</li>
+      <li>Zucchini</li></p></div>
+
   </div> <!-- END TAB CONTENT -->
 </div>	<!-- COLUMN -->	
 		</div> <!-- END PANEL BODY -->
