d68834092912ea554019aee6dd78785246bcf2bd
Brian Baker <mrbrianbaker@users.noreply.github.com>
2015-08-20 15:58:57 -0700

    updated text

--- about.php
+++ about.php
@@ -10,12 +10,15 @@
           <h3 class="panel-title">About Us</h3>
         </div>
         <div class="panel-body">
-          <img class="img-responsive" src="http://www.brpres.org/FarmersMarketPics/BRPYouthFarmersMarket%20012.jpg" width="1120" height="500" alt="Harvest Posse" class="image-inline" title="Farmers Market" />
+          <img class="img-responsive" src="http://www.brpres.org/FarmersMarketPics/BRPYouthFarmersMarket%20012.jpg" width="1120" height="500" alt="Harvest Posse" class="image-inline" title="Farmers Market" width="100" height="100" />
 <p style="text-align: center; ">First day Farmers Market in Seattle, 2015</p>
 <h3 style="text-align: center; ">About Us</h3>
-<div id="parent-fieldname-text"><p><span>The Neighborhood Farmers Market Alliance is committed to supporting and strengthening Washington's small family farm businesses by creating and operating vibrant, successful neighborhood farmers markets. We are simply another resource coming up underneath the Farmers Markets of Seattle and providing support and answers for the consumers in this beautiful city! </span></p>
-<p><span>We provide effective direct sales sites for our region's small farmers, while also educating consumers about local farm products and the importance of supporting and preserving local farmland. In addition, we have been at the forefront of advocating for low income benefits programs to ensure equal food access. We also provide answers to the typical consumers questions.</span></p>
-<p><span><a href="#">Here</a> you will find plenty of information regarding what is available this beautiful summer season at each Neighborhood Market!</span></p>
+<div id="parent-fieldname-text"><p>Have you ever found yourself searching for when a particular piece of produce is in season at your local market? Have you ever been shopping at your local market and found yourself disappointed because none of the vendors had what you needed? 
+				</p>
+				<p>We are here to answer questions just like these! We are here to tell you what produce is available at which Seattle Neighborhood Market and when. Keeping track of what produce will be at what market in each season is hard enough – let us help you! 
+				</p>
+				<p><a href="fresh.php">Here</a> you will find plenty of information regarding what is available this beautiful summer season at each Neighborhood Market! 
+				</p>
 <p><strong>Timeline:</strong></p>
 <p><strong>2015</strong> - Our Harvest Posse Farmers Market was orgine by local volunteers from Seattle.</p>
 
